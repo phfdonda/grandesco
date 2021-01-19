3877ad3474c8f1ee9f3e96f51f9eb14c06b614c1
Pedro Donda <phfdonda@gmail.com>
2021-01-19 18:48:27 -0300

    Add breakpoints style sheet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav>
-      <Link to="/developers">Developers</Link>
-      <Link to="/supervisors">Supervisors</Link>
-      <Link to="/companies">Companies</Link>
+        <Link to="/developers">Developers</Link>
+        <Link to="/supervisors">Supervisors</Link>
+        <Link to="/companies">Companies</Link>
     </nav>
   )
 }
